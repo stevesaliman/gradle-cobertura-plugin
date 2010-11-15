fe4bffc6bd19f127283ca4101665a3141e47de7f
Val Kolovos <valkolovos@hotmail.com>
2010-11-15 15:38:07 -0600

    first add of files

---
+++ src/main/groovy/com/orbitz/gradle/cobertura/tasks/GenerateCoverageReportTask.groovy
@@ -0,0 +1,19 @@
+package com.orbitz.gradle.cobertura.tasks
+
+
+import org.gradle.api.internal.ConventionTask;
+import org.gradle.api.tasks.TaskAction;
+
+class GenerateCoverageReportTask extends ConventionTask {
+    
+    String format
+    File datafile
+    String reportdir
+    Set<File> srcDirs
+    def runner
+    
+    @TaskAction
+    def generateCoverageReport() {
+        runner.generateCoverageReport getDatafile().path, getReportdir(), getFormat(), getSrcDirs().collect { it.path }
+    }
+}
