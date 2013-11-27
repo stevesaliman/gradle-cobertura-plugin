2098b30915382010e9f107ef6dc774c56e9a5711
Steven C. Saliman <steve.saliman@gmail.com>
2013-11-26 19:12:03 -0700

    Added a couple more tests

--- src/test/groovy/net/saliman/gradle/plugin/cobertura/CoberturaPluginExecutionTest.groovy
+++ src/test/groovy/net/saliman/gradle/plugin/cobertura/CoberturaPluginExecutionTest.groovy
@@ -6,6 +6,9 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.fail
+
 /**
  * Created with IntelliJ IDEA.
  * User: steve
@@ -17,17 +20,20 @@ import org.junit.Test
  */
 class CoberturaPluginExecutionTest {
 	ProjectConnection connection
+	String[] stdout
+	String[] stderr
 
 	@Before
 	void setUp() {
 		connection = GradleConnector.newConnector()
-		.forProjectDirectory(new File("testclient/calculator"))
-		.connect()
+						.forProjectDirectory(new File("testclient/calculator"))
+						.connect()
 	}
 
 	@After
 	void tearDown() {
 		if ( connection != null ) {
+			System.out.println("Closing connection")
 			connection.close()
 		}
 	}
@@ -35,13 +41,75 @@ class CoberturaPluginExecutionTest {
 
 	@Test
 	void execute() {
-//		connection.model(CustomModel)
-		def result = connection.newBuild()
-						.withArguments("-m", "-b", "build.gradle")
-						.forTasks("clean").run()
-//		ModelBuilder gradleBuilder = connection.model(GradleProject.class);
-//	def model = connection.model(CustomModel.class)
-//		def project = model.getName()
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+							.withArguments("-m", "-b", "build.gradle")
+							.forTasks("clean")
+							.setStandardOutput(outputStream)
+							.setStandardError(errorStream)
+							.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertExecuted(":buildSrc:test")
+		assertUpToDate(":buildSrc:assemble")
+		assertSkipped(":calculator:clean")
 		System.out.println("here")
+	}
+
+	def assertExecuted(String task) {
+		if ( stdout == null || stdout.length < 1 ) {
+			fail "Standard Output not set. Did Gradle run?"
+		}
+		for ( String s : stdout ) {
+			// if the line is just the task name, it executed.
+			if ( s.equals(task) ) {
+				return
+			}
+			// If the line starts with the task name and a space, it was either
+			// skipped or up to date.  The assert statement just gives us a nice
+			// message.
+			if ( s.startsWith("${task} ") ) {
+				assertEquals("${task} did not execute.", task, s)
+				return  // we know it executed, return.
+			}
+		}
+		fail "${task} was not in the output, so it did not execute."
+	}
+
+	def assertSkipped(String task) {
+		if ( stdout == null || stdout.length < 1 ) {
+			fail "Standard Output not set. Did Gradle run?"
+		}
+		for ( String s : stdout ) {
+			if ( s.startsWith("${task} ") ) {
+				// need 2 Groovy Strings...
+				assertEquals("${task} did not execute.", "${task} SKIPPED", "${s}")
+				return // we know it was skipped, return
+			}
+		}
+		fail "${task} was not in the output, is it a valid task?"
+	}
+
+	def assertUpToDate(String task) {
+		if ( stdout == null || stdout.length < 1 ) {
+			fail "Standard Output not set. Did Gradle run?"
+		}
+		for ( String s : stdout ) {
+			if ( s.startsWith("${task} ") ) {
+				// Need 2 Groovy strings...
+				assertEquals("${task} did not execute.", "${task} UP-TO-DATE", "${s}")
+				return // we know it was skipped, return
+			}
+		}
+		fail "${task} was not in the output, is it a valid task?"
 	}
 }
