3e545ceb4dcae9963388eb34e524ee9555d9157b
Steven C. Saliman <steve.saliman@gmail.com>
2014-12-14 15:14:21 -0700

    Beefed up the unit tests to prove what tasks run when users request different things

--- src/test/groovy/net/saliman/gradle/plugin/cobertura/CoberturaPluginExecutionTest.groovy
+++ src/test/groovy/net/saliman/gradle/plugin/cobertura/CoberturaPluginExecutionTest.groovy
@@ -10,11 +10,8 @@ import static org.junit.Assert.assertEquals
 import static org.junit.Assert.fail
 
 /**
- * Created with IntelliJ IDEA.
- * User: steve
- * Date: 11/18/13
- * Time: 7:52 PM
- * To change this template use File | Settings | File Templates.
+ * This class tries to do actual builds under various circumstances to determine
+ * which tasks actually run from run to run.
  *
  * @author Steven C. Saliman
  */
@@ -38,13 +35,14 @@ class CoberturaPluginExecutionTest {
 		}
 	}
 
-
-	@Test
-	void execute() {
+	/**
+	 * Helper task to start with a clean slate.
+	 */
+	void executeClean() {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
 		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
 		connection.newBuild()
-							.withArguments("-m", "-b", "build.gradle")
+							.withArguments("-b", "build.gradle")
 							.forTasks("clean")
 							.setStandardOutput(outputStream)
 							.setStandardError(errorStream)
@@ -59,9 +57,205 @@ class CoberturaPluginExecutionTest {
 		if ( s != null ) {
 			stderr = s.split('\n')
 		}
-		assertExecuted(":buildSrc:test")
-		assertUpToDate(":buildSrc:assemble")
-		assertSkipped(":calculator:clean")
+		System.out.println("here")
+	}
+
+	/**
+	 * Execute tests without specifying any cobertura tasks.  This should cause
+	 * tests to execute, but no cobertura related tasks.
+	 */
+	@Test
+	void executeTestNoCobrtura() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-b", "build.gradle", "-x", "testDivide")
+						.forTasks("test")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertSkipped(":calculator:instrument")
+		assertSkipped(":calculator:copyCoberturaDatafile")
+		assertExecuted(":calculator:test")
+		assertSkipped(":calculator:generateCoberturaReport")
+		assertSkipped(":calculator:performCoverageCheck")
+		System.out.println("here")
+	}
+
+	/**
+	 * Execute the cobertura task.  This should cause tests to run.  It should
+	 * also cause instrumentation and report generation, but not coverage
+	 * checking.
+	 */
+	@Test
+	void executeCobertura() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-b", "build.gradle", "-x", "testDivide")
+						.forTasks("cobertura")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertExecuted(":calculator:instrument")
+		assertExecuted(":calculator:copyCoberturaDatafile")
+		assertExecuted(":calculator:test")
+		assertExecuted(":calculator:generateCoberturaReport")
+		assertSkipped(":calculator:performCoverageCheck")
+		System.out.println("here")
+	}
+
+	/**
+	 * Execute the cobertura in a dry run.  Nothing should run in this case.
+	 */
+	@Test
+	void executeCoberturaDryRun() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-m", "-b", "build.gradle")
+						.forTasks("cobertura")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertSkipped(":calculator:instrument")
+		assertSkipped(":calculator:copyCoberturaDatafile")
+		assertSkipped(":calculator:test")
+		assertSkipped(":calculator:generateCoberturaReport")
+		assertSkipped(":calculator:performCoverageCheck")
+		System.out.println("here")
+	}
+
+	/**
+	 * Test report generation when we build with the "test" and "coberturaReport"
+	 * tasks.  This should do the same thing as running "cobertura" alone.
+	 */
+	@Test
+	void executeTestCoberturaReport() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-b", "build.gradle", "-x", "testDivide")
+						.forTasks("test", "coberturaReport")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertExecuted(":calculator:instrument")
+		assertExecuted(":calculator:copyCoberturaDatafile")
+		assertExecuted(":calculator:test")
+		assertExecuted(":calculator:generateCoberturaReport")
+		assertSkipped(":calculator:performCoverageCheck")
+		System.out.println("here")
+	}
+
+	/**
+	 * Test checking coverage when we build with the "test" and "checkCoverage"
+	 * tasks.  This should do the same thing as running "cobertura" alone, but
+	 * with the addition of the coverage check task.
+	 */
+	@Test
+	void executeTestCoberturaCheck() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-b", "build.gradle", "-x", "testDivide")
+						.forTasks("test", "coberturaCheck")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertExecuted(":calculator:instrument")
+		assertExecuted(":calculator:copyCoberturaDatafile")
+		assertExecuted(":calculator:test")
+		assertExecuted(":calculator:generateCoberturaReport")
+		assertExecuted(":calculator:performCoverageCheck")
+		System.out.println("here")
+	}
+
+	/**
+	 * Test checking coverage when we build with the "test" and "checkCoverage"
+	 * tasks, but we are doing a dry run. No tasks should run.
+	 */
+	@Test
+	void executeTestCoberturaCheckDryRun() {
+		executeClean();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+		ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
+		connection.newBuild()
+						.withArguments("-m", "-b", "build.gradle", "-x", "testDivide")
+						.forTasks("test", "coberturaCheck")
+						.setStandardOutput(outputStream)
+						.setStandardError(errorStream)
+						.run()
+		outputStream.close()
+		errorStream.close()
+		String s = outputStream.toString()
+		if ( s != null ) {
+			stdout = s.split('\n')
+		}
+		s = errorStream.toString()
+		if ( s != null ) {
+			stderr = s.split('\n')
+		}
+		assertSkipped(":calculator:instrument")
+		assertSkipped(":calculator:copyCoberturaDatafile")
+		assertSkipped(":calculator:test")
+		assertSkipped(":calculator:generateCoberturaReport")
+		assertSkipped(":calculator:performCoverageCheck")
 		System.out.println("here")
 	}
 
